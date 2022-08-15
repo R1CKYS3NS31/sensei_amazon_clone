e194c1278ce08173849c9b2cd7d67bdf64a80318
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-15 16:29:13 +0300

    home commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './component/header/Header';
+import { Home } from './component/home/Home';
 
 function App() {
   return (
     <div className="app">
     <Header/>
-    {/* home */}
+    <Home/>
+    
     </div>
   );
 }
---
+++ src/component/product/Product.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './product.css'
+
+export const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
