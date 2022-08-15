29bb50c8868e3679e239b96c9dd12970cc54ea0a
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-15 13:34:03 +0300

    header commit

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 export const Header = () => {
   return (
-    <div className='header'>
-        <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+    <div className="header">
+      <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        {/* logo */}
+      </div>
+      <div className="header_nav"></div>
     </div>
-  )
-}
+  );
+};
