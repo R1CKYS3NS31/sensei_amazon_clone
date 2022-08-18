7cc58a9546aafb93525a4b1267a7ca62c0e2592c
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-18 11:32:50 +0300

    product commit

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -11,13 +11,16 @@ export const Product = () => {
           <strong>19.99</strong>
         </p>
         <div className="product_rating">
-            <p>🌟</p>
-            <p>🌟</p>
-            <p>🌟</p>
+          <p>🌟</p>
+          <p>🌟</p>
+          <p>🌟</p>
         </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymog?UnL._AC_SY400_.jpg" alt="p_img" />
-        <button>Add to Basket</button>
       </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt="p_img"
+      />
+      <button>Add to Basket</button>
     </div>
   );
 };
