4ca5022572608ba9d2f6984115d890d24f09e8f2
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-18 17:06:44 +0300

    checkout subtotal commit

--- src/component/checkout/subtotal/Subtotal.jsx
+++ src/component/checkout/subtotal/Subtotal.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import CurrencyFormat from 'react-currency-format'
-import './subtotal.css'
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import "./subtotal.css";
 
 export const Subtotal = () => {
   return (
     <div className="subtotal">
-        <CurrencyFormat
-        renderText={(value)=>{
-            <>
-            <p>Subtotal ({basket.length} items)</p></>
-        }}/>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small className="subtotal_gift">
+              <input type="checkbox" name="gift" id="gift" />
+              This order contains a gift
+            </small>
+          </>
+  )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"Ksh. "}
+      />
+      
     </div>
-  )
-}
+  );
+};
