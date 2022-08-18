4a465c96b94ebc4a8c9d7c1cc20da7125606839e
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-18 17:20:21 +0300

    checkout subtotal commit

--- src/component/checkout/subtotal/Subtotal.jsx
+++ src/component/checkout/subtotal/Subtotal.jsx
@@ -12,7 +12,7 @@ export const Subtotal = () => {
               Subtotal (0 items): <strong>0</strong>
             </p>
             <small className="subtotal_gift">
-              <input type="checkbox" name="gift" id="gift" />
+              <input type="checkbox" name="gift" id="" />
               This order contains a gift
             </small>
           </>
@@ -23,6 +23,7 @@ export const Subtotal = () => {
         thousandSeparator={true}
         prefix={"Ksh. "}
       />
+      <button>Proceed to checkout</button>
     </div>
   );
 };
