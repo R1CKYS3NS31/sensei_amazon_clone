cfb168614cc9cb42e9a9feed8dc49cf5634a3b22
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-18 14:32:43 +0300

    checkout subtotal commit

--- src/component/checkout/subtotal/Subtotal.jsx
+++ src/component/checkout/subtotal/Subtotal.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import CurrencyFormat from 'react-currency-format'
 import './subtotal.css'
 
 export const Subtotal = () => {
   return (
-    <div>Subtotal</div>
+    <div className="subtotal">
+        <CurrencyFormat
+        renderText={(value)=>{
+            <>
+            <p>Subtotal ({basket.length} items)</p></>
+        }}/>
+    </div>
   )
 }
