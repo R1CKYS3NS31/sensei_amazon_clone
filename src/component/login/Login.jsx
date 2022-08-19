e993295124b5d9a594ace15f29a619b58b747184
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-19 18:22:06 +0300

    login commit

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './login.css'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+        <Link to={'/'}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt="" className="login_logo" />
+        </Link>
+    </div>
   )
 }
