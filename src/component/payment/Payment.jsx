799c9b43ad11882cbf17f2bc5272946fe5828fb0
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-08-20 04:11:54 +0300

    payment commit

--- src/component/payment/Payment.jsx
+++ src/component/payment/Payment.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useStateValue } from "../../utils/StateProvider";
+import { CheckoutProduct } from "../checkout/checkoutProduct/CheckoutProduct";
+import "./payment.css";
 
 export const Payment = () => {
-    return (
-        <div>
-            Payment
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="payment">
+      <div className="payment_container">
+        {/* payment and delivery */}
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment_address">
+            <p>{user?.email}</p>
+            <p>Nakuru East</p>
+            <p>Bondeni</p>
+          </div>
         </div>
-    )
-}
+        {/* payment section - review items */}
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment_items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
